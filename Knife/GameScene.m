60f22d2d0348fec23551e312426475030d3a147c
ldi <ldi@purdue.edu>
2015-03-07 15:20:25 -0500

    add knife and knife animation

--- Knife/GameScene.m
+++ Knife/GameScene.m
@@ -18,15 +18,25 @@
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
+//    self.physicsWorld.gravity = CGVectorMake( 0.0, -5.0 );
     /* Setup your scene here */
     SKTexture* handTexture = [SKTexture textureWithImageNamed:@"hand.jpg"];
     handTexture.filteringMode = SKTextureFilteringNearest;
+    SKTexture* groundTexture = [SKTexture textureWithImageNamed:@"ground.png"];
+    groundTexture.filteringMode = SKTextureFilteringNearest;
+
+    SKAction* moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:0.02 * groundTexture.size.width*2];
+    SKAction* resetGroundSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0];
+    SKAction* moveGroundSpritesForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
     
     SKTexture* knifeTexture = [SKTexture textureWithImageNamed:@"knife.png"];
     _knife = [SKSpriteNode spriteNodeWithTexture:knifeTexture];
     [_knife setScale:0.5];
     _knife.position = CGPointMake(self.frame.size.width / 4 + 400, CGRectGetMidY(self.frame));
-    _knife.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_knife.size];
+//    _knife.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:_knife.size.height/2];
+//    _knife.physicsBody.dynamic = YES;
+//    _knife.physicsBody.allowsRotation = NO;
+    
     
     _hand = [SKSpriteNode spriteNodeWithTexture:handTexture];
     [_hand setScale:1.0];
@@ -34,8 +44,23 @@
     _skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
     [self setBackgroundColor:_skyColor];
     
+    for( int i = 0; i < 2 + self.frame.size.width / ( groundTexture.size.width * 2 ); ++i ) {
+        SKSpriteNode* sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
+        [sprite setScale:2.0];
+        sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2);
+        [sprite runAction:moveGroundSpritesForever];
+
+        [self addChild:sprite];
+    }
+    
+    SKNode* dummy = [SKNode node];
+    dummy.position = CGPointMake(0, groundTexture.size.height);
+    dummy.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.frame.size.width, groundTexture.size.height * 2)];
+    dummy.physicsBody.dynamic = NO;
     [self addChild:_hand];
     [self addChild:_knife];
+    [self addChild:dummy];
+
     }
 
 -(id)initWithSize:(CGSize)size {
@@ -57,8 +82,11 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    [_knife.physicsBody applyImpulse:CGVectorMake(0, 8)];
-    
+    CGPoint location = [[touches anyObject] locationInNode:self];
+    location.x = location.x + _knife.size.width/2;
+    location.y = location.y - _knife.size.height/2;
+    SKAction *move = [SKAction moveTo:location duration:0.2f];
+    [_knife runAction:move];
 
 }
 
