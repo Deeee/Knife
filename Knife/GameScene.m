7087328942d16bea9e9ed4877036d4662a77eaf6
ldi <ldi@purdue.edu>
2015-03-07 14:49:19 -0500

    add knife and knife's physical body

--- Knife/GameScene.m
+++ Knife/GameScene.m
@@ -10,6 +10,8 @@
 
 @interface GameScene () {
     SKSpriteNode* _hand;
+    SKSpriteNode* _knife;
+    SKColor* _skyColor;
 }
 @end
 
@@ -20,13 +22,21 @@
     SKTexture* handTexture = [SKTexture textureWithImageNamed:@"hand.jpg"];
     handTexture.filteringMode = SKTextureFilteringNearest;
     
+    SKTexture* knifeTexture = [SKTexture textureWithImageNamed:@"knife.png"];
+    _knife = [SKSpriteNode spriteNodeWithTexture:knifeTexture];
+    [_knife setScale:0.5];
+    _knife.position = CGPointMake(self.frame.size.width / 4 + 400, CGRectGetMidY(self.frame));
+    _knife.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_knife.size];
+    
     _hand = [SKSpriteNode spriteNodeWithTexture:handTexture];
     [_hand setScale:1.0];
     _hand.position = CGPointMake(self.frame.size.width / 4 + 200, CGRectGetMidY(self.frame));
-    
+    _skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
+    [self setBackgroundColor:_skyColor];
     
     [self addChild:_hand];
-}
+    [self addChild:_knife];
+    }
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
@@ -38,7 +48,8 @@
         _hand = [SKSpriteNode spriteNodeWithTexture:handTexture];
         [_hand setScale:2.0];
         _hand.position = CGPointMake(self.frame.size.width / 4, CGRectGetMidY(self.frame));
-        
+        _skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
+        [self setBackgroundColor:_skyColor];
         [self addChild:_hand];
     }
     return self;
@@ -46,6 +57,7 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
+    [_knife.physicsBody applyImpulse:CGVectorMake(0, 8)];
     
 
 }
