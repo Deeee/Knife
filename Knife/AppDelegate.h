13c0a26571c1af3b31e060f59c0fe6deeac57547
ldi <ldi@purdue.edu>
2015-03-07 14:13:24 -0500

    init
    init

---
+++ Knife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Knife
+//
+//  Created by Liu Di on 3/6/15.
+//  Copyright (c) 2015 Liu Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Knife/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Knife
+//
+
+//  Copyright (c) 2015 Liu Di. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
